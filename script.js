f376173df856232ae9a6acaafaa44b5b5dbd817f
KacyClark <kacy@usa.com>
2022-05-17 03:51:28 -0400

    Creating variables
    Creating variables using var, const and let

---
+++ script.js
@@ -0,0 +1,18 @@
+     //alert("Hello World");
+    //promt("Enter your name:");
+console.log("This is a message");
+console.warn("This is a warning message");
+console.error("This is an error message");
+let userAge = 99;
+userAge = 98;
+document.writeln(userAge);
+let userName = "Kyle";
+document.writeln(`<p>The users name is ${userName}, and the age is ${userAge}</p>`);
+
+let jobTitle = "future FS engineer";
+let geoLocation = "Kentucky";
+const partnerName = "None";
+
+let numberChildren = 2;
+console.log(`<p>I will be a ${jobTitle} in ${geoLocation}, and married to ${partnerName} with 
+${numberChildren} kids.</p>`);
